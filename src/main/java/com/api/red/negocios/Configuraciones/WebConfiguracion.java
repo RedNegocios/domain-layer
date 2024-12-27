0b574ebbb0bd19e37a3052eb6c8b5087413ddf71
Rodrigo <rodri.mo@ciencias.unam.mx>
2024-12-26 20:45:32 -0600

    Agregamos funcionalidad correcta de configuracion CORS

--- src/main/java/com/api/red/negocios/Configuraciones/WebConfiguracion.java
+++ src/main/java/com/api/red/negocios/Configuraciones/WebConfiguracion.java
@@ -1,7 +1,5 @@
 package com.api.red.negocios.Configuraciones;
 
-
-import org.apache.logging.log4j.core.Logger;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -16,7 +14,7 @@ public class WebConfiguracion implements WebMvcConfigurer {
     	
     	System.out.println("llegamos al ok papuuufgurfbrfbnv");
         registry.addMapping("/**") // Aplica a todos los endpoints
-                .allowedOrigins("http://localhost:8080") // Permite solicitudes desde React
+                .allowedOrigins("http://localhost:3000") // Permite solicitudes desde React
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
                 .allowedHeaders("*") // Permite todos los encabezados
                 .allowCredentials(true); // Permite el envío de cookies si es necesario
